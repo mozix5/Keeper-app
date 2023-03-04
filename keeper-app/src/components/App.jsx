e52a295f8e0231f699b19230bfd923899e20d559
mozix5 <76263118+mozix5@users.noreply.github.com>
2023-03-04 20:02:07 +0530

    Update App.jsx

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,23 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 function App() {
   const [newNote, setNewNote] = useState([]);
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("notes")) || [];
+    setNewNote(savedNotes);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(newNote));
+  }, [newNote]);
+
   function addNote(note) {
     setNewNote((prevNote) => {
       return [...prevNote, note];
     });
   }
+
   function deleteNote(id) {
-    console.log(id);
     setNewNote((prevNote) => {
       return prevNote.filter((value, index) => {
         return index !== id;
       });
     });
   }
+
   return (
     <div>
       <Header />
